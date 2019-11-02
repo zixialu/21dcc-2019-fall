b923f337e5ce73da82b2334849ad3c5796b19b6c
Zixia Lu <zixia@brokerbay.ca>
2019-11-02 15:37:46 -0400

    Day 2

---
+++ 2/index.js
@@ -0,0 +1,8 @@
+const interviewAnswer = (topic) => {
+  switch (topic) {
+    case 'arts funding': return "We'll have to get creative!";
+    case 'economy': return "Time is money.";
+    case 'transportation': return "It's going to be a long road, so we better get moving.";
+    default: return "No comment.";
+  }
+}
